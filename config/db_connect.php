227d44793a73cc9a54991eaf22286602d04bd9ae
Bhagya Patel <60622922+bhagya2002@users.noreply.github.com>
2020-04-08 23:33:15 -0600

    comments added to organize

--- config/db_connect.php
+++ config/db_connect.php
@@ -4,5 +4,6 @@ $conn = mysqli_connect('localhost', 'bhagya', 'test1234', 'todolist');
 
 // check connection 
 if (!$conn) {
+    // if the conncetion fails to conenct to the server
     echo 'Connection error: ' . mysqli_connect_error();
 }
