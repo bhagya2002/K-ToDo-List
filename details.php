ffb2fd1db9d9fa6afd2a8fc81bc16b91bb62e15e
Bhagya Patel <60622922+bhagya2002@users.noreply.github.com>
2020-04-08 23:44:40 -0600

    added comments

--- details.php
+++ details.php
@@ -1,18 +1,21 @@
 <?php
 session_start();
+// connect to the database
 include('config/db_connect.php');
 
-
+// this is run when the form is submit
 if (isset($_POST['delete'])) {
     $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
 
+    // crete SQL to delete rows
     $sql = "DELETE FROM addtodo WHERE id = $id_to_delete";
 
+    // connection and finalize no errors
     if (mysqli_query($conn, $sql)) {
-        //success
+        //success and redirect
         header("Location: signed.php");
     } {
-        //failure
+        //failure and display error
         echo 'query error: ' . mysqli_error(($conn));
     }
 }
@@ -73,10 +76,18 @@ if (isset($_GET['id'])) {
             }
         }
     </style>
+
+    <!-- body  -->
+    <body>
+
+    <!-- header for the title -->
     <nav class="white z-depth-0">
         <div class="container">
+        <!-- header -->
             <a href="signed.php" id="title" class="brand-logo brand-text">ToDoList</a>
+            <!-- "nav bar" -->
             <ul id="nav-mobile" class="right">
+            <!-- button to add a to-do -->
                 <li><a href="addtodo.php" class="btn brand z-depth-0">Add a ToDo</a></li>
             </ul>
         </div>
